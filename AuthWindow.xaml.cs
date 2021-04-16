a41e3ff696e59d6d5c426b4f35d06e22a3b4ae6c
Tivasic <68960327+Tivasic@users.noreply.github.com>
2021-04-16 16:34:35 +0300

    Creating a Main Window, adding a User Control

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestUsers
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        db db;
+        public AuthWindow()
+        {
+            InitializeComponent();
+            db = new db();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Login.Text.Trim();
+            string password = Password.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                Login.ToolTip = "Введите логин";
+                Login.BorderBrush = Brushes.Red;
+                Login.CaretBrush = Brushes.Red;
+            }
+            else
+            {
+                Login.ToolTip = null;
+                Login.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#89000000"));
+            }
+
+            if (password.Length < 5)
+            {
+                Password.ToolTip = "Введите пароль";
+                Password.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                Password.ToolTip = null;
+                Password.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#89000000"));
+            }
+
+            if (login.Length >= 5)
+            {
+                if (password.Length >= 5)
+                {
+                    User authUser = null;
+                    using (db context = new db())
+                    {
+                        authUser = db.Users.Where(b => b.Login == login && b.Password == password).FirstOrDefault();
+
+                    }
+
+                    if (authUser != null)
+                    {
+                        MessageBox.Show("Пользователь авторизовался");
+                    }
+                    else MessageBox.Show("Пользователь не найден");
+                }
+            }
+        }
+
+        private void Button_Window_Register_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow registerWindow = new RegisterWindow();
+            registerWindow.Show();
+            Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,70 +16,33 @@ using System.Windows.Shapes;
 namespace TestUsers
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class MainWindow : Window
     {
-        db db;
         public MainWindow()
         {
             InitializeComponent();
-            db = new db();
         }
 
-        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string login = Login.Text.Trim();
-            string password = Password.Password.Trim();
+            UserControl usc = null;
+            GridMain.Children.Clear();
 
-            if (login.Length < 5)
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
             {
-                Login.ToolTip = "Введите логин";
-                Login.BorderBrush = Brushes.Red;
-                Login.CaretBrush = Brushes.Red;
+                case "ItemHome":
+                    usc = new UserControlHome();
+                    GridMain.Children.Add(usc);
+                    break;
+                case "ItemCreate":
+                    usc = new UserControlCreate();
+                    GridMain.Children.Add(usc);
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                Login.ToolTip = null;
-                Login.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#89000000"));
-            }
-
-            if (password.Length < 5)
-            {
-                Password.ToolTip = "Введите пароль";
-                Password.BorderBrush = Brushes.Red;
-            }
-            else
-            {
-                Password.ToolTip = null;
-                Password.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#89000000"));
-            }
-
-            if (login.Length >= 5)
-            {
-                if (password.Length >= 5)
-                {
-                    User authUser = null;
-                    using (db context = new db())
-                    {
-                        authUser = db.Users.Where(b => b.Login == login && b.Password == password).FirstOrDefault();
-
-                    }
-
-                    if (authUser != null)
-                    {
-                        MessageBox.Show("Пользователь авторизовался");
-                    }
-                    else MessageBox.Show("Пользователь не найден");
-                }
-            }
-        }
-
-        private void Button_Window_Register_Click(object sender, RoutedEventArgs e)
-        {
-            RegisterWindow registerWindow = new RegisterWindow();
-            registerWindow.Show();
-            Close();
         }
     }
 }
